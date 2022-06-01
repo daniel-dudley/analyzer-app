acefdde939114d1b9738eefab8e2c292fa31bee7
Daniel Dudley <dandudley@me.com>
2022-05-31 19:48:30 -0700

    Adapt code to work with assay design v4

--- analyzer.py
+++ analyzer.py
@@ -82,34 +82,19 @@ def analyze():
 
     # note: im.crop(x-left, y-top, x-right, y-bottom)
     # crop left region and rgb color swatches
-    #testing_region = 
-    #control_region =
-    r_swatch = im_4.crop((0.045*width, 0.0496*height, 0.3075*width, 0.565*height)).save(r"images/r_swatch.jpg")
-    #g_swatch =
-    #b_swatch =
-
-
-    '''
-    # note: im.crop(x-left, y-top, x-right, y-bottom)
-    # crop left region and rgb color swatches
-    region_left = im.crop((850, 320, 1210, 1000)).save(r"images/region_left.jpg")
-    r_swatch_left = im.crop((800, 110, 900, 210)).save(r"images/r_swatch_left.jpg")
-    g_swatch_left = im.crop((965, 120, 1060, 210)).save(r"images/g_swatch_left.jpg")
-    b_swatch_left = im.crop((1120, 125, 1220, 210)).save(r"images/b_swatch_left.jpg")
-
-    # crop right region and rgb color swatches
-    region_right = im.crop((1340, 320, 1740, 970)).save(r"images/region_right.jpg")
-    r_swatch_right = im.crop((1325, 130, 1415, 220)).save(r"images/r_swatch_right.jpg")
-    g_swatch_right = im.crop((1480, 130, 1570, 220)).save(r"images/g_swatch_right.jpg")
-    b_swatch_right = im.crop((1640, 140, 1730, 220)).save(r"images/b_swatch_right.jpg")
+    testing_region = im_4.crop((0.55*width, 0.15*height, 0.78*width, 0.28*height)).save(r"images/testing_region.jpg")
+    control_region = im_4.crop((0.23*width, 0.74*height, 0.41*width, 0.87*height)).save(r"images/control_region.jpg")
+    r_swatch = im_4.crop((0.074*width, 0.51*height, 0.26*width, 0.55*height)).save(r"images/r_swatch.jpg")
+    g_swatch = im_4.crop((0.42*width, 0.50*height, 0.61*width, 0.55*height)).save(r"images/g_swatch.jpg")
+    b_swatch = im_4.crop((0.77*width, 0.51*height, 0.96*width, 0.55*height)).save(r"images/b_swatch.jpg")
 
 
     #################
     # rgb averaging #
     #################
     def average(image_path):
-        im_3 = Image.open(image_path)
-        stat = ImageStat.Stat(im_3)
+        im_5 = Image.open(image_path)
+        stat = ImageStat.Stat(im_5)
         rgb = stat.mean
         
         r, g, b = rgb
@@ -117,29 +102,18 @@ def analyze():
         g = round(g)
         b = round(b)
         rgb_average = [r, g, b]
-        #print(rgb_average)
+        
         return rgb_average
 
-    #print("region_left_avg: ")
-    region_left_avg = average(r"images/region_left.jpg")
-    #print("r_swatch_left_avg: ")
-    r_swatch_left_avg = average(r"images/r_swatch_left.jpg")
-    #print("g_swatch_left_avg: ")
-    g_swatch_left_avg = average(r"images/g_swatch_left.jpg")
-    #print("b_swatch_left_avg: ")
-    b_swatch_left_avg = average(r"images/b_swatch_left.jpg")
+    testing_region_avg = average(r"images/testing_region.jpg") #region_left_avg
+    control_region_avg = average(r"images/control_region.jpg") #r_swatch_left_avg
+    r_swatch_avg = average(r"images/r_swatch.jpg") #r_swatch_left_avg
+    g_swatch_avg = average(r"images/g_swatch.jpg") #g_swatch_left_avg
+    b_swatch_avg = average(r"images/b_swatch.jpg") #b_swatch_left_avg
 
-    #print("region_right_avg: ")
-    region_right_avg = average(r"images/region_right.jpg")
-    #print("r_swatch_right_avg: ")
-    r_swatch_right_avg = average(r"images/r_swatch_right.jpg")
-    #print("g_swatch_right_avg: ")
-    g_swatch_right_avg = average(r"images/g_swatch_right.jpg")
-    #print("b_swatch_right_avg: ")
-    b_swatch_right_avg = average(r"images/b_swatch_right.jpg")
-
-    #print("region left average: ", region_left_avg)
-    #print("region left average: ", region_right_avg)
+    #print("testing region avg: ", testing_region_avg)
+    #print("control region avg: ", control_region_avg)
+    #print(r_swatch_avg, g_swatch_avg, b_swatch_avg)
 
 
     ####################
@@ -151,14 +125,14 @@ def analyze():
     b_pure = [57, 83, 164]
 
     # calculate rgb weights (% difference from pure) of left test
-    r_weight_left = 1 + ((r_pure[0] - r_swatch_left_avg[0]) / r_pure[0])
-    g_weight_left = 1 + ((g_pure[1] - g_swatch_left_avg[1]) / g_pure[1])
-    b_weight_left = 1 + ((b_pure[2] - b_swatch_left_avg[2]) / b_pure[2])
+    r_correction_coefficient = 1 + ((r_pure[0] - r_swatch_avg[0]) / r_pure[0]) #r_weight_left
+    g_correction_coefficient = 1 + ((g_pure[1] - g_swatch_avg[1]) / g_pure[1])
+    b_correction_coefficient = 1 + ((b_pure[2] - b_swatch_avg[2]) / b_pure[2])
 
     # calculate rgb weights of right test
-    r_weight_right = 1 + ((r_pure[0] - r_swatch_right_avg[0]) / r_pure[0])
-    g_weight_right = 1 + ((g_pure[1] - g_swatch_right_avg[1]) / g_pure[1])
-    b_weight_right = 1 + ((b_pure[2] - b_swatch_right_avg[2]) / b_pure[2])
+    #r_weight_right = 1 + ((r_pure[0] - r_swatch_right_avg[0]) / r_pure[0])
+    #g_weight_right = 1 + ((g_pure[1] - g_swatch_right_avg[1]) / g_pure[1])
+    #b_weight_right = 1 + ((b_pure[2] - b_swatch_right_avg[2]) / b_pure[2])
 
     # print(r_weight_left, g_weight_left, b_weight_left)
     # print(r_weight_right, g_weight_right, b_weight_right)
@@ -168,12 +142,12 @@ def analyze():
     # calculate biomarker concentration #
     #####################################
     # CysC calibration curve data
-    r_int_CysC = 313.4999999999999
-    r_slope_CysC = -167.49999999999991
-    g_int_CycC = 376.8571428571427
-    g_slope_CysC = -193.57142857142847
-    b_int_CysC = 346.07142857142856
-    b_slope_CysC = -133.21428571428572
+    #r_int_CysC = 313.4999999999999
+    #r_slope_CysC = -167.49999999999991
+    #g_int_CycC = 376.8571428571427
+    #g_slope_CysC = -193.57142857142847
+    #b_int_CysC = 346.07142857142856
+    #b_slope_CysC = -133.21428571428572
 
     # Alb calibration curve data
     r_int_Alb = 286.4285714285713
@@ -184,20 +158,21 @@ def analyze():
     b_slope_Alb = -1.8999999999999952
 
     # calculate the concentration of left uPAD (CysC) and apply color correction
-    r_concentration_left = ((r_weight_left * region_left_avg[0]) - r_int_CysC) / r_slope_CysC
-    g_concentration_left = ((g_weight_left * region_left_avg[1]) - g_int_CycC) / g_slope_CysC
-    b_concentration_left = ((b_weight_left * region_left_avg[2]) - b_int_CysC) / b_slope_CysC
+    #r_concentration_left = ((r_weight_left * region_left_avg[0]) - r_int_CysC) / r_slope_CysC
+    #g_concentration_left = ((g_weight_left * region_left_avg[1]) - g_int_CycC) / g_slope_CysC
+    #b_concentration_left = ((b_weight_left * region_left_avg[2]) - b_int_CysC) / b_slope_CysC
 
     # calculate the concentration of right uPAD (Alb) and apply color correction
-    r_concentration_right = ((r_weight_right * region_right_avg[0]) - r_int_Alb) / r_slope_Alb
-    g_concentration_right = ((g_weight_right * region_right_avg[1]) - g_int_Alb) / g_slope_Alb
-    b_concentration_right = ((b_weight_right * region_right_avg[2]) - b_int_Alb) / b_slope_Alb
+    r_concentration = ((r_correction_coefficient * testing_region_avg[0]) - r_int_Alb) / r_slope_Alb
+    g_concentration = ((g_correction_coefficient * testing_region_avg[1]) - g_int_Alb) / g_slope_Alb
+    b_concentration = ((b_correction_coefficient * testing_region_avg[2]) - b_int_Alb) / b_slope_Alb
 
     # calculate average conentration of all color bands
-    rgb_concentration_left = (r_concentration_left + g_concentration_left + b_concentration_left) / 3
-    rgb_concentration_right = (r_concentration_right + g_concentration_right + b_concentration_right) / 3
+    rgb_concentration = (r_concentration + g_concentration + b_concentration) / 3
+    #rgb_concentration_right = (r_concentration_right + g_concentration_right + b_concentration_right) / 3
 
-    return str(rgb_concentration_left), str(rgb_concentration_right)
+    #return str(rgb_concentration)
+    print(rgb_concentration)
 
     # display [biomarker] values
     #print("\n")
@@ -206,6 +181,7 @@ def analyze():
     #print("The concentraiton of albumin in mg/g = ")
     #print(str(rgb_concentration_right))
 
+'''
 rgb_concentration_left, rgb_concentration_right = analyze()
 
 print(rgb_concentration_left)
